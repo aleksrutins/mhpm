f8d92d54a5d55fe4eb65ef4fcc37ed857f6bd040
munchkinhalfling <alex@rutins.com>
2018-12-21 09:44:16 -0500

    It WORKS!!!

---
+++ mhpm.js
@@ -0,0 +1,31 @@
+(function() { // Closure for mhpm library
+    let mhpmRepoJson;
+    let mhpmRepoLoaded = false;
+    let request = url => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.send();
+        return xhr.responseText;
+    }
+    let requestJson = (url) => {
+        
+        return JSON.parse(request(url));
+    }
+    (function() { // Closure for Module
+        class Module {
+            static load(name) {
+                if(!mhpmRepoLoaded) {
+                    console.info("mhpm: loading repo file");
+                    mhpmRepoJson = requestJson('http://localhost:8000/repository.json');
+                    mhpmRepoLoaded = true;
+                    console.info("mhpm: repo file loaded");
+                }
+                console.info("mhpm: installing package '" + name + "'");
+                let pkg = mhpmRepoJson.pkgs.find((_pkg) => _pkg.name === name);
+                let pkgConfFile = requestJson(pkg["conf-file"]);
+                return eval(request(pkgConfFile.pkgfile));
+            }
+        }
+        window.Module = Module;
+    })();
+})();
---
+++ testpkg/testpkg.js
@@ -0,0 +1,7 @@
+(function() {
+    return {
+        sayHi() {
+            alert("hi");
+        }
+    }
+})()
